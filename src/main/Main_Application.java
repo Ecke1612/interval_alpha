5748f8ed100cd6f9cce86a5e4784885d76ae82ec
Eike Dreyer <eikedreyer@gmx.de>
2018-05-23 21:12:22 +0200

    Erinnerungsfunktion eingefügt

--- src/main/Main_Application.java
+++ src/main/Main_Application.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 
 public class Main_Application extends Application {
 
-    public static final String build = "0.68";
+    public static final String build = "0.681";
 
     private final String fn = "SourceSansPro-";
     private final String[] fonts = {"uiicons.ttf", fn+"Black.tff", fn+"BlackItalic.tff", fn+"Bold.tff",
